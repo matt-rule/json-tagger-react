0107349a7df31a19566b1f135acf1c404489ac3a
matt-rule <matt.rule.github@gmail.com>
2020-05-26 04:22:32 +0100

    React now handles JSON data from API

--- src/components/image-list/image-list-json.tsx
+++ src/components/image-list/image-list-json.tsx
@@ -4,29 +4,41 @@ import './site.css';
 import './image-list-json.css';
 
 const API_STRING = 'http://localhost:5000/imagelist';
-  
-class ImageListJson extends React.Component<{}, { json:string }> {
+
+type ImageListWebResult = {
+  origFilePath: string;
+  thumb: string;
+}
+
+class ImageListJson extends React.Component<{}, ImageListWebResult[]> {
     constructor(props : any) {
       super(props);
-   
-      this.state = {
-        json: "",
-      };
+
+      this.state = [] as ImageListWebResult[];
+      //this.state = [{"origFilePath": "test", "thumb": "test2"},{"origFilePath": "test3", "thumb": "test4"}];
+      // this.state = {
+      //   json: "",
+      // };
     }
 
     componentDidMount() {
         fetch(API_STRING)
-          .then(
-              response => response.json()
-              .then (x => x as string)
-          )
-          .then(json => this.setState({ json }));
-      }
+          .then(response => response.json())
+          .then(x => x as ImageListWebResult[])
+          .then(json => this.setState(json));
+    }
 
     render() {
+        let state = this.state;
+        const fileMetadataFromApi : Array<ImageListWebResult> = [];
+        Object.keys(state).forEach(function(i : any) {
+          fileMetadataFromApi.push(state[i] as ImageListWebResult);
+        });
+        let jsonString : string = JSON.stringify(fileMetadataFromApi);
+
         return (
             <div className='image-list-json'>
-                { this.state.json }
+                { jsonString }
             </div>
         );
     }
