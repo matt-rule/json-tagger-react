bea9e65793a0715a1612aae53075ad5a8a167c53
matt-rule <matt.rule.github@gmail.com>
2020-05-31 15:19:14 +0100

    Fix thumbnails not appearing sometimes

--- src/components/file-list/file-grid.tsx
+++ src/components/file-list/file-grid.tsx
@@ -12,7 +12,7 @@ const FileGrid = ({fileMetadataArray, functionToCall} : {fileMetadataArray : Arr
                 fileMetadataArray
                 .map((x, i) =>
                     <Col key={x.guid} xs={2}>
-                    <img alt='thumbnail' src={'file/thumbnails/thumb_' + x.guid + '.jpg'} onClick={e => functionToCall(i)} />
+                    <img alt='thumbnail' src={'/file/thumbnails/thumb_' + x.guid + '.jpg'} onClick={e => functionToCall(i)} />
                     <p className='no-wrap-text' onClick={e => functionToCall(i)}>
                         {x.origFilePath}
                     </p>
